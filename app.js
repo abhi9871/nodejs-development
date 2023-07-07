04b94228a64c8d48bcd4e030ea2c9ad75cb843f7
abhi9871 <at719273@gmail.com>
2023-07-07 15:21:07 +0530

    Return custom responses based on different url

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Abhishek');
-    res.end('Abhishek');
+    const url = req.url;
+    if(url === '/home')
+    {
+        res.write('Welcome home');
+        return res.end();
+    }
+    if(url === '/about')
+    {
+        res.write('Welcome to About Us page');
+        return res.end();
+    }
+    if(url === '/node')
+    {
+        res.write('Welcome to my Node Js project');
+        return res.end();
+    }
+
 });
 
 server.listen(4000, () => {
