8aaf2c595612d507b480d191882f4b11f7928022
abhi9871 <at719273@gmail.com>
2023-07-12 13:45:48 +0530

    Create separate files for each routes

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    res.send(`<html><head><title>My form</title></head><body><form action = "/admin/add-product" method = "POST"><input type = "text" name = "title"><input type = "text" name = "size"><button type = "submit">Add Product</button></form></body></html>`);
+})
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/shop');
+})
+
+module.exports = router;
