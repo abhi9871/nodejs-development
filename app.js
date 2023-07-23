3dcc9c21a9167a364cf0d5b11f62a448ed9f2ae3
abhi9871 <at719273@gmail.com>
2023-07-24 03:12:09 +0530

    Association b/w cart, product and user added

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const userRoutes = require('./routes/user');
 const expenseRoutes = require('./routes/expense');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 const cors = require('cors');
 
 app.use(cors());
@@ -40,9 +42,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User); // optional
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
  
 sequelize
-  // .sync({ force: true })
+//   .sync({ force: true })
   .sync()
   .then(result => {
     return User.findByPk(1);
@@ -56,6 +62,9 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then(() => {
     app.listen(3000);
   })
   .catch(err => {
